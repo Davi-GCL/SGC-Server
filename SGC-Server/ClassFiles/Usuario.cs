6e1bbab4d58f3bd2f344db431e68b9386d12d013
davi.lemos <1davi.gabriel1@gmail.com>
2023-10-03 13:33:02 -0300

    Delete classes geradas

---
+++ SGC-Server/ClassFiles/Usuario.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+namespace siscom;
+
+public class USUARIO
+{
+ 	double CODIGOUSUARIO { get; set; }
+	string NOMECOMPLETOUSUARIO { get; set; }
+	string NOMEREDUZIDOUSUARIO { get; set; }
+	DateTime DATASTATUSUSUARIO { get; set; }
+	string LOGINAD { get; set; }
+	string MATRICULAUSUARIO { get; set; }
+	string EMAILUSUARIO { get; set; }
+	double CODIGOAREA { get; set; }
+	string PAPELAREA { get; set; }
+	string CODIGOSTATUSUSUARIO { get; set; }
+	double CODIGOPERFIL { get; set; }
+	DateTime DATACADASTRO { get; set; }
+	double USUARIOCADASTRO { get; set; }
+	DateTime DATAULTIMAATUALIZACAO { get; set; }
+	double USUARIOULTIMAATUALIZACAO { get; set; }
+	double IDFILIAL { get; set; }
+ 
+}
+
